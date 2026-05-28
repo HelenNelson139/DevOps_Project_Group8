a183c9e92fbafccfc1de78114f49ca225997b102
HelenNelson139 <23521023@gm.uit.edu.vn>
2026-05-29 02:51:02 +0700

    Configure Terraform remote state backend

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -3,12 +3,17 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name           = "default"
     node_count     = var.node_count
     vm_size        = var.vm_size
     vnet_subnet_id = var.vnet_subnet_id
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   identity {
